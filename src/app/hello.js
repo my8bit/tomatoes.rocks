bf025b478ce7f385c5eeb674a34c208a8534e77e
my8bit <mail@my8bit.name>
2016-10-16 12:40:34 +0300

    initial pomodoro

---
+++ src/app/hello.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+
+export class TimerWidget extends Component {
+  constructor() {
+    super();
+    this.state = {time: 25, running: false, buttonName: 'Start'};
+  }
+  action() {
+    if (this.state.running) {
+      clearInterval(this.interval);
+      this.reset();
+    } else {
+      this.interval = setInterval(this.changeTime.bind(this), 1000);
+      this.start();
+    }
+  }
+  reset() {
+    this.setState({time: 25, running: false, buttonName: 'Start'});
+  }
+  start() {
+    this.setState({running: true, buttonName: 'Reset'});
+  }
+  timer() {
+    this.interval = setInterval(this.changeTime.bind(this), 1000);
+  }
+  changeTime() {
+    const time = this.state.time - 1;
+    this.setState({time});
+  }
+  render() {
+    let {time, buttonName} = this.state;
+    return (
+      <div>
+        <div id="countdown">{time}</div>
+        <button onClick={this.action.bind(this)}>{buttonName}</button>
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import {TimerWidget} from './app/hello';
+
+import './index.scss';
+
+ReactDOM.render(
+  <TimerWidget/>,
+  document.getElementById('container')
+);
