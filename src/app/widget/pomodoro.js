540556c6482dcebf4ebcfb268868d05700e40beb
my8bit <mail@my8bit.name>
2016-10-16 18:58:36 +0300

    Auth test

--- src/app/widget/pomodoro.js
+++ src/app/widget/pomodoro.js
@@ -1,5 +1,53 @@
 import React, {Component} from 'react';
+import Authlify from 'authlify-js'
 
+const authlify = new Authlify({
+  APIUrl: 'https://authlify.netlify.com'
+});
+
+authlify.signup("my8bit", "mail@my8bit.name").then(
+  response => console.log("Confirmation email sent", response),
+  error => console.log("Error during signup: %o", error.msg)
+);
+/*
+authlify.confirm(token).then(
+  (user) => console.log("Logged in as %s", user.email),
+  (error) => console.log("Failed to log in: %o", error)
+);
+
+authlify.login(email, password).then(
+  (user) => console.log("Logged in as %s", user.email),
+  (error) => console.log("Failed to log in: %o", error)
+)
+
+authlify.requestPasswordRecovery(email).then(
+  (response) => console.log("Recovery email sent"),
+  (error) => console.log("Error sending recovery mail: %o", error)
+);
+
+authlify.recover(token).then(
+  (user) => console.log("Logged in as %s", user.email),
+  (error) => console.log("Failed to verify recover token: %o", error)
+);
+*/
+const user = authlify.currentUser();
+console.log(user);
+/*
+user.update({email: newEmail, password: newPassword}).then(
+  (user) => console.log("Updated user"),
+  (error) => console.log("Failed to update user: %o", error)
+);
+
+user.jwt().then(
+  (token) => console.log("Current token: %s", token),
+  (error) => console.log("Failed to get token: %o", error)
+);
+
+user.logout().then(
+  (response) => console.log("User logged out"),
+  (error) => console.log("Failed to logout user: %o", error)
+);
+*/
 export class TimerWidget extends Component {
   constructor() {
     super();
