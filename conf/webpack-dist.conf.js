e3dd02c2c240909f8a2079f7c273e40732d03a45
my8bit <mail@my8bit.name>
2016-10-16 15:26:07 +0300

    move webpack to depe

--- conf/webpack-dist.conf.js
+++ conf/webpack-dist.conf.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const SplitByPathPlugin = require('webpack-split-by-path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
 
 module.exports = {
