17a1138fad7871d093842c902002bf7d27ad665a
my8bit <mail@my8bit.name>
2016-11-02 09:34:41 +0200

    commened offline support in prod

--- conf/webpack-dist.conf.js
+++ conf/webpack-dist.conf.js
@@ -6,7 +6,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const SplitByPathPlugin = require('webpack-split-by-path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const autoprefixer = require('autoprefixer');
-const OfflinePlugin = require('offline-plugin');
+// const OfflinePlugin = require('offline-plugin');
 
 module.exports = {
   module: {
@@ -55,11 +55,11 @@ module.exports = {
       path: path.join(__dirname, '../node_modules')
     }]),
     new ExtractTextPlugin('/index-[contenthash].css'),
-    new OfflinePlugin({
+    //new OfflinePlugin({
       //ServiceWorker: {
       //  output: 'static'
       //}
-    })
+    //})
   ],
   postcss: () => [autoprefixer],
   output: {
