07295819bbe3a5015096ccbbd7fb8d99d153eabc
my8bitname <mail@my8bit.name>
2017-08-31 14:27:08 +0000

    fix browsersync configuration

--- conf/browsersync.conf.js
+++ conf/browsersync.conf.js
@@ -5,29 +5,31 @@ const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const webpackConf = require('./webpack.conf');
 const webpackBundler = webpack(webpackConf);
-const options = {
-  server: {
-    baseDir: [
-      conf.paths.tmp,
-      conf.paths.src
-    ],
-    middleware: [
-      webpackDevMiddleware(webpackBundler, {
+
+module.exports = () => {
+  const options = {
+    server: {
+      baseDir: [
+        conf.paths.tmp,
+        conf.paths.src
+      ],
+      middleware: [
+        webpackDevMiddleware(webpackBundler, {
           // IMPORTANT: dev middleware can't access config, so we should
           // provide publicPath by ourselves
-        publicPath: webpackConf.output.publicPath,
+          publicPath: webpackConf.output.publicPath,
 
           // Quiet verbose output in console
-        quiet: true
-      }),
+          quiet: true
+        }),
 
         // bundler should be the same as above
-      webpackHotMiddleware(webpackBundler)
-    ]
-  },
-  open: false
-};
-const https = process.env.TOMATOES_SERVER_KEY &&
+        webpackHotMiddleware(webpackBundler)
+      ]
+    },
+    open: false
+  };
+  const https = process.env.TOMATOES_SERVER_KEY &&
               process.env.TOMATOES_SERVER_CRT ? {
                 https: {
                   key: path.resolve(process.env.TOMATOES_SERVER_KEY),
@@ -35,13 +37,10 @@ const https = process.env.TOMATOES_SERVER_KEY &&
                 }
               } : {};
 
-const address = process.env.PORT &&
+  const address = process.env.PORT &&
                 process.env.IP ? {
                   port: process.env.PORT,
                   host: process.env.IP
                 } : {};
-
-module.exports = function () {
-  console.log(Object.assign(options, https, address));
   return Object.assign(options, https, address);
 };
