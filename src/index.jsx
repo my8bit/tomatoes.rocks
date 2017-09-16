54ed9be71e3d7ea56caadf7a71c7e52d9bdcb74b
my8bit <mail@my8bit.name>
2017-09-17 01:04:43 +0200

    test without offline plugin

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
-import Offline from 'offline-plugin/runtime';
-Offline.install();
+// import Offline from 'offline-plugin/runtime';
+// Offline.install();
 
 // TODO: https://codelabs.developers.google.com/codelabs/add-to-home-screen/#5
 // TODO: https://developer.chrome.com/multidevice/android/installtohomescreen
