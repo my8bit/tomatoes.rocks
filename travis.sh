820221766d2e6fbe5e602254cb7e1dbd0f337155
my8bit <mail@my8bit.name>
2017-12-16 17:17:09 +0100

    pass env parameters to travis

--- travis.sh
+++ travis.sh
@@ -1,6 +1,6 @@
 # HEAD is BRANCH NAME uncomment to get branch name locally
 # this variable exist in deployement
-# HEAD=$(git rev-parse --abbrev-ref HEAD)
+HEAD=$(git rev-parse --abbrev-ref HEAD)
 
 curl -X POST \
   https://api.travis-ci.org/repo/my8bit%2Ftomatoes.work/requests \
@@ -8,4 +8,4 @@ curl -X POST \
   -H "authorization: token $TRAVIS_TOKEN" \
   -H 'content-type: application/json' \
   -H 'travis-api-version: 3' \
-  -d '{"branch": "'"$HEAD"'"}'
+  -d '{"branch": "'"$HEAD"'", "config": {"env": {"TRAVIS_EVENT_TYPE": "pull_request", "URL": "DEPLOY_PRIME_URL"}}}'
