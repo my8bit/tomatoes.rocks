6bab905807d1dc6f0828fb5e4902483e0c63b6b4
my8bit <mail@my8bit.name>
2017-12-16 15:57:56 +0100

    skip install

--- travis.sh
+++ travis.sh
@@ -2,6 +2,13 @@
 # this variable exist in deployement
 # BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
+echo $REPOSITORY_URL
+echo $BRANCH
+echo $PULL_REQUEST
+echo $HEAD
+echo $COMMIT_REF
+echo $CONTEXT
+
 curl -X POST \
   https://api.travis-ci.org/repo/my8bit%2Ftomatoes.work/requests \
   -H 'accept: application/json' \
