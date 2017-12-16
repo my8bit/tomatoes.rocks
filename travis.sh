302ba78a102ad40aee860af7cf56c211a74db6a4
my8bit <mail@my8bit.name>
2017-12-16 19:17:25 +0100

    try new travis config

--- travis.sh
+++ travis.sh
@@ -2,36 +2,26 @@
 # this variable exist in deployement
 # HEAD=$(git rev-parse --abbrev-ref HEAD)
 
-echo "REPOSITORY_URL"
-echo $REPOSITORY_URL
-echo "BRANCH"
-echo $BRANCH
-echo "PULL_REQUEST"
-echo $PULL_REQUEST
-echo "HEAD"
-echo $HEAD
-echo "COMMIT_REF"
+PULL_REQUEST_NUMBER=grep -Eo '[0-9]{1,4}' <<< $BRANCH
+echo $PULL_REQUEST_NUMBER
 echo $COMMIT_REF
-echo "CONTEXT"
-echo $CONTEXT
-
-# curl -X POST \
-#   https://lighthouse-ci.appspot.com/run_on_chrome \
-#   -H 'content-type: application/json' \
-#   -H 'x-api-key: '"$LIGHTHOUSE_API_KEY"'' \
-#   -d '{
-#   "testUrl": "'"$DEPLOY_PRIME_URL"'",
-#   "output": "json",
-#   "addComment": true,
-#   "repo": {
-#     "owner": "my8bit",
-#     "name": "tomatoes.work"
-#   },
-#   "pr": {
-#     "number": 13,
-#     "sha": "e51f9a8611d0c70d7f1b5d21899cdaa3f5a0478e"
-#   }
-# }'
+curl -X POST \
+  https://lighthouse-ci.appspot.com/run_on_chrome \
+  -H 'content-type: application/json' \
+  -H 'x-api-key: "'"$LIGHTHOUSE_API_KEY"'"' \
+  -d '{
+  "testUrl": "'"$DEPLOY_PRIME_URL"'",
+  "output": "json",
+  "addComment": true,
+  "repo": {
+    "owner": "my8bit",
+    "name": "tomatoes.work"
+  },
+  "pr": {
+    "number": "'"$PULL_REQUEST_NUMBER"'",
+    "sha": "'"$COMMIT_REF"'"
+  }
+}'
 
 
 # curl -X POST \
