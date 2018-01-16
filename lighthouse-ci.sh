33f51b9d0aa1347ff72c175cacc2264176cdba80
my8bit <mail@my8bit.name>
2018-01-16 22:39:12 +0100

    add webhook for lighthouse

--- lighthouse-ci.sh
+++ lighthouse-ci.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-curl -X POST \
-  https://lighthouse-ci.appspot.com/run_on_chrome \
-  -H 'content-type: application/json' \
-  -H 'x-api-key: "'"$LIGHTHOUSE_API_KEY"'"' \
-  -d '{
+WEBHOOK_BODY = '{
     "testUrl": "'"$DEPLOY_PRIME_URL"'",
     "output": "json",
     "addComment": true,
@@ -17,3 +13,21 @@ curl -X POST \
       "sha": "'"$COMMIT_REF"'"
     }
   }'
+
+# curl -X POST \
+#   https://wt-c4bac0445c2fe7d18676b1c5286ab6c7-0.run.webtask.io/lighthouse \
+#   -H 'content-type: application/json' \
+#   -H 'x-api-key: "'"$LIGHTHOUSE_API_KEY"'"' \
+#   -d '{
+#     "testUrl": "'"$DEPLOY_PRIME_URL"'",
+#     "output": "json",
+#     "addComment": true,
+#     "repo": {
+#       "owner": "my8bit",
+#       "name": "tomatoes.work"
+#     },
+#     "pr": {
+#       "number": "'"$(grep -Eo '[0-9]{1,4}' <<< $BRANCH)"'",
+#       "sha": "'"$COMMIT_REF"'"
+#     }
+#   }'
